f0032b21e0fd7397f1d95aa4c66f57a57b6196ce
Eduard <hadiev.e@yandex.ru>
2023-10-07 21:23:10 +0300

    fix: test PR

--- src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
+++ src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
@@ -27,8 +27,11 @@ public class ItemDto {
     private String description;
     @NotNull
     private Boolean available;
+    @JsonIgnore
     private List<CommentDto> comments;
+    @JsonIgnore
     private BookingDtoShort lastBooking;
+    @JsonIgnore
     private BookingDtoShort nextBooking;
     @JsonIgnore
     private long ownerId;
